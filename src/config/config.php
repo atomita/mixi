4b9c8d42fb7058d04bb86f992229ad29307da04e
atomita <atom.at.omita@gmail.com>
2013-11-26 21:14:52 +0900

    add config.php

---
+++ src/config/config.php
@@ -0,0 +1,9 @@
+<?php
+
+return array(
+	'consumer_key'		 => 'Your Consumer Key',
+	'consumer_secret'	 => 'Your Consumer Secret',
+	'scope'				 => 'Your Scope', // optional
+	'redirect_uri'		 => 'RedirectURI', // optional
+	'display'			 => 'Display Type (pc, touch, smartphone or ios)', // optional
+);

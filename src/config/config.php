539ce02d0dd912bdba6c84da6590c6244200c9b8
atomita <atom.at.omita@gmail.com>
2013-11-27 19:51:05 +0900

    optional is comment out. add comment.

--- src/config/config.php
+++ src/config/config.php
@@ -1,9 +1,28 @@
 <?php
 
 return array(
-	'consumer_key'		 => 'Your Consumer Key',
-	'consumer_secret'	 => 'Your Consumer Secret',
-	'scope'				 => 'Your Scope', // optional
-	'redirect_uri'		 => 'RedirectURI', // optional
-	'display'			 => 'Display Type (pc, touch, smartphone or ios)', // optional
+	/*
+	 * required. Your Consumer Key.
+	 */
+	'consumer_key'		 => '',
+	
+	/*
+	 * required. Your Consumer Secret.
+	 */
+	'consumer_secret'	 => '',
+
+	/*
+	 * optional. Your Scope.
+	 */
+//	'scope'				 => '',
+
+	/*
+	 * optional. Redirect URI.
+	 */
+//	'redirect_uri'		 => '', 
+
+	/*
+	 * optional. Display Type (pc, touch, smartphone or ios).
+	 */
+//	'display'			 => '',
 );
